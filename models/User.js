337d6eebc41e878db96326dd03bb5b13b6f2edbc
wooopers <kngordon@msn.com>
2023-07-28 12:32:11 -0500

    User.js

--- models/User.js
+++ models/User.js
@@ -1,4 +1,27 @@
 const User = require('./User');
 const Post = require('./Reactions');
 
+username:
+String
+Unique
+Required
+Trimmed
+
+email:
+String
+Unique
+Required
+Trimmed
+//Must match a valid email address (look into Mongoose's matching validation)
+
+thoughts:
+//Array of _id values referencing the Thought model
+
+friends:
+//Array of _id values referencing the User model (self-reference)
+
+Schema Settings:
+
+//Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
 module.exports = { User, Post };
